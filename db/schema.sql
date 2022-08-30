223c45b5fc78e0f2a9a14448d37aa11d0c90e3fa
Stephen Heinen <stephenheinen22@yahoo.com>
2022-08-30 03:32:23 -0500

    set up models and schema.sql but I might not need the set up in schema? not sure, leaving for now

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,36 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+-- use db
+USE ecommerce_db;
+
+-- tables organized from readme
+CREATE TABLE category (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  category_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE product (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  product_name VARCHAR(30) NOT NULL,
+  price DECIMAL NOT NULL,
+  stock INT NOT NULL DEFAULT(10),
+  category_id INT, 
+  FOREIGN KEY (category_id)
+  REFERENCES category(id)
+);
+
+CREATE TABLE tag (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  tag_name VARCHAR(30)
+);
+
+CREATE TABLE productTag (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  tag_id INT,
+  product_id INT,
+  FOREIGN KEY (product_id)
+  REFERENCES product(id),
+  FOREIGN KEY (tag_id)
+  REFERENCES tag(id)
+);
--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,30 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isInt: true,
+      }
+    }
   },
   {
     sequelize,
